80deec2a920ffe24981a3130911b60dd84d99fa7
yuhang1008 <cnhanyuhang@outlook.com>
2025-09-07 14:54:13 +0800

    support blackwell gpu

--- setup.py
+++ setup.py
@@ -27,9 +27,18 @@ setup(
             "spatial.cu", 
             "simple_knn.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": [], "cxx": cxx_compiler_flags})
+            extra_compile_args={"nvcc": ["-gencode=arch=compute_60,code=sm_60",
+                                          "-gencode=arch=compute_61,code=sm_61",
+                                          "-gencode=arch=compute_70,code=sm_70",
+                                          "-gencode=arch=compute_75,code=sm_75",
+                                          "-gencode=arch=compute_80,code=sm_80",
+                                          "-gencode=arch=compute_86,code=sm_86",
+                                          "-gencode=arch=compute_89,code=sm_89",
+                                          "-gencode=arch=compute_90,code=sm_90",
+                                          "-gencode=arch=compute_120,code=sm_120"], "cxx": cxx_compiler_flags})
         ],
     cmdclass={
         'build_ext': BuildExtension
-    }
+    },
+    options={'build_ext': {'parallel': 16}}
 )
